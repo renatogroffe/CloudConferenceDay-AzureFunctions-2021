6801419b58b7454ef698b9e7c511573dfa9cdbdd
Renato Groffe <renato.groffe@yahoo.com.br>
2021-09-23 10:51:01 -0300

    Exemplos em Azure Functions + .NET 5 para o Cloud Conference Day
    Exemplos em Azure Functions + .NET 5 para o Cloud Conference Day

---
+++ FunctionAppProcessarAcoes/Acoes.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using FunctionAppProcessarAcoes.Data;
+using FunctionAppProcessarAcoes.Models;
+
+namespace FunctionAppProcessarAcoes
+{
+    public class Acoes
+    {
+        private readonly AcoesRepository _repository;
+
+        public Acoes(AcoesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Function("Acoes")]
+        [OpenApiOperation(operationId: "Acoes", tags: new[] { "Acoes" }, Summary = "Acao", Description = "Consultar as cotações de Ações.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Acao[]), Summary = "Consulta de cotações de Ações.", Description = "Consulta de cotações de Ações.")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("Acoes");
+            
+            var historicoAcoes = _repository.GetAll();
+            logger.LogInformation($"No. de documentos encontrados: {historicoAcoes.Count()}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(historicoAcoes);
+            return response;
+        }
+    }
+}
